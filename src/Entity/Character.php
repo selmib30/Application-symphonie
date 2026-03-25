427bd51188dda4548a9d90208190f936c12b2fb0
papynoel <clementbreuil123@gmail.com>
2026-03-25 09:14:57 +0100

    modif perso

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -86,4 +86,175 @@ class Character
         $this->parties = new ArrayCollection();
     }
 
+    // --- GETTERS & SETTERS AJOUTÉS CI-DESSOUS ---
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): static
+    {
+        $this->level = $level;
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): static
+    {
+        $this->strength = $strength;
+        return $this;
+    }
+
+    public function getDexterity(): ?int
+    {
+        return $this->dexterity;
+    }
+
+    public function setDexterity(int $dexterity): static
+    {
+        $this->dexterity = $dexterity;
+        return $this;
+    }
+
+    public function getConstitution(): ?int
+    {
+        return $this->constitution;
+    }
+
+    public function setConstitution(int $constitution): static
+    {
+        $this->constitution = $constitution;
+        return $this;
+    }
+
+    public function getIntelligence(): ?int
+    {
+        return $this->intelligence;
+    }
+
+    public function setIntelligence(int $intelligence): static
+    {
+        $this->intelligence = $intelligence;
+        return $this;
+    }
+
+    public function getWisdom(): ?int
+    {
+        return $this->wisdom;
+    }
+
+    public function setWisdom(int $wisdom): static
+    {
+        $this->wisdom = $wisdom;
+        return $this;
+    }
+
+    public function getCharisma(): ?int
+    {
+        return $this->charisma;
+    }
+
+    public function setCharisma(int $charisma): static
+    {
+        $this->charisma = $charisma;
+        return $this;
+    }
+
+    public function getHealthPoints(): ?int
+    {
+        return $this->healthPoints;
+    }
+
+    public function setHealthPoints(int $healthPoints): static
+    {
+        $this->healthPoints = $healthPoints;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getRace(): ?Race
+    {
+        return $this->race;
+    }
+
+    public function setRace(?Race $race): static
+    {
+        $this->race = $race;
+        return $this;
+    }
+
+    public function getCharacterClass(): ?CharacterClass
+    {
+        return $this->characterClass;
+    }
+
+    public function setCharacterClass(?CharacterClass $characterClass): static
+    {
+        $this->characterClass = $characterClass;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Party>
+     */
+    public function getParties(): Collection
+    {
+        return $this->parties;
+    }
+
+    public function addParty(Party $party): static
+    {
+        if (!$this->parties->contains($party)) {
+            $this->parties->add($party);
+        }
+        return $this;
+    }
+
+    public function removeParty(Party $party): static
+    {
+        $this->parties->removeElement($party);
+        return $this;
+    }
 }
